c4cc1dafdca745bec1bb63b446922d2b70a64187
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-16 15:36:36 +0700

    Contact page view

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './views/Home'
 import About from './views/About'
+import Contact from './views/Contact'
 
 const App = () => {
   return(
     <div>
       <Route path="/" exact component={Home}/>
       <Route path="/About" exact component={About}/>
+      <Route path="/Contact" exact component={Contact}/>
     </div>
   )
 }
