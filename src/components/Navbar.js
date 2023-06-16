5db2354c4b701560bca6798498038fd41d3dea67
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-16 14:21:10 +0700

    Navbar, footer, and home component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return(
+        <div>
+            <h1>This is Header</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
