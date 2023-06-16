b87c006bae3326cc28ebe625abccdc82248f2366
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-16 16:29:49 +0700

    Route not found

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from '../views/Home'
 import About from '../views/About'
 import Contact from '../views/Contact'
 import Detail from '../views/Detail'
+import NotFound from '../views/NotFound'
 
 const Routes = () => {
     return (
         <React.Fragment>
-            <Route path="/" exact component={Home} />
-            <Route path="/About" exact component={About} />
-            <Route path="/Contact" exact component={Contact} />
-            <Route path="/Detail/:id" exact component={Detail} />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/About" exact component={About} />
+                <Route path="/Contact" exact component={Contact} />
+                <Route path="/Detail/:id" exact component={Detail} />
+                <Route component={NotFound} />
+            </Switch>
         </React.Fragment>
     )
 }
